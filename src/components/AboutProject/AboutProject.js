88b4de6f6f865a6bf0e9bde95a0906cdb1bcea0b
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-03-05 21:58:28 +0300

    promo and navtab adapted, aboutProject changed in grids

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -6,24 +6,20 @@ function AboutProject(props) {
     <section className="about-project">
       <h2 className="about-project__title">О проекте</h2>
       <div className="about-project__columns">
-        <div className="about-project__column">
-          <p className="about-project__column-title">
-            Дипломный проект включал 5 этапов
-          </p>
-          <p className="about-project__column-content">
-            Составление плана, работу над бэкендом, вёрстку, добавление
-            функциональности и финальные доработки.
-          </p>
-        </div>
-        <div className="about-project__column">
-          <p className="about-project__column-title">
-            На выполнение диплома ушло 5 недель
-          </p>
-          <p className="about-project__column-content">
-            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
-            соблюдать, чтобы успешно защититься.
-          </p>
-        </div>
+        <p className="about-project__column-title">
+          Дипломный проект включал 5 этапов
+        </p>
+        <p className="about-project__column-title">
+          На выполнение диплома ушло 5 недель
+        </p>
+        <p className="about-project__column-content">
+          Составление плана, работу над бэкендом, вёрстку, добавление
+          функциональности и финальные доработки.
+        </p>
+        <p className="about-project__column-content">
+          У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+          соблюдать, чтобы успешно защититься.
+        </p>
       </div>
       <div className="about-project__stages">
         <div className="about-project__stage about-project_dark">
