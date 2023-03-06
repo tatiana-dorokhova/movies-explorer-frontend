4d1ba407eec7c98dab88d9d4ad52e795a3a0fe66
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-03-06 20:36:54 +0300

    aboutMe changed to grid and adapted

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,14 +8,18 @@ function AboutMe(props) {
       <h2 className="about-me__title">Студент</h2>
       <div className="about-me__columns">
         <div className="about-me__info">
-          <div className="about-me__name">Татьяна</div>
-          <p className="about-me__job">Начинающий фронтенд-разработчик</p>
-          <p className="about-me__description">
-            Здесь последует длинное жизнеописание для проверки того, как
-            переносятся слова и добавляются новые строки по мере роста опыта.
-            Три-четыре строки в двести символов длиной вполне достаточно, чтобы
-            оценить масштаб и проверить позиционирование блока на общем фоне.
-          </p>
+          <div className="about-me__info-block">
+            <div className="about-me__name">Татьяна</div>
+            <p className="about-me__job">Начинающий фронтенд-разработчик</p>
+            <p className="about-me__description">
+              Я долгое время жила и работала в Новосибирске и Москве, закончила
+              мехмат НГУ. Люблю посещать театры и концерты, увлекаюсь дайвингом,
+              походами, парапланами, мотоциклами. Больше десяти лет работала в
+              Яндексе, но кодить начала совсем недавно. После курса по
+              веб-разработке продолжу работать в IT, хочу начать с автотестов и
+              плавно перебраться в продакшн-разработку.
+            </p>
+          </div>
           <a
             className="about-me__github-link"
             href="https://github.com/tatiana-dorokhova"
