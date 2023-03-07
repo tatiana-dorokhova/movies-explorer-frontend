cbed55e39fd94083ae4914a1709e459894a252ed
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-03-07 16:18:08 +0300

    footer changed to grid and adapted

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,27 +7,25 @@ function Footer(props) {
       <p className="footer__description">
         Учебный проект Яндекс.Практикум х BeatFilm.
       </p>
-      <div className="footer__columns">
-        <div className="footer__copyright">© 2023</div>
-        <nav className="footer__links">
-          <a
-            className="footer__link"
-            href="https://practicum.yandex.ru"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Яндекс.Практикум
-          </a>
-          <a
-            className="footer__link"
-            href="https://github.com/tatiana-dorokhova"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-        </nav>
-      </div>
+      <div className="footer__copyright">© 2023</div>
+      <nav className="footer__links">
+        <a
+          className="footer__link footer__link_practicum"
+          href="https://practicum.yandex.ru"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Яндекс.Практикум
+        </a>
+        <a
+          className="footer__link footer__link_github"
+          href="https://github.com/tatiana-dorokhova"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </nav>
     </div>
   );
 }
