8c5cbef2d69b46bc35689f363ebaf566924d5f85
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-03-09 15:11:39 +0300

    footer deleted from some pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,33 +1,44 @@
 // презентационный компонент, который отрисовывает подвал
 import './Footer.css';
+import { useLocation } from 'react-router-dom';
 
 function Footer(props) {
-  return (
-    <div className="footer">
-      <p className="footer__description">
-        Учебный проект Яндекс.Практикум х BeatFilm.
-      </p>
-      <div className="footer__copyright">© 2023</div>
-      <nav className="footer__links">
-        <a
-          className="footer__link footer__link_practicum"
-          href="https://practicum.yandex.ru"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Яндекс.Практикум
-        </a>
-        <a
-          className="footer__link footer__link_github"
-          href="https://github.com/tatiana-dorokhova"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </nav>
-    </div>
-  );
+  const location = useLocation();
+
+  if (
+    location.pathname === '/' ||
+    location.pathname === '/movies' ||
+    location.pathname === '/saved-movies'
+  ) {
+    return (
+      <div className="footer">
+        <p className="footer__description">
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </p>
+        <div className="footer__copyright">© 2023</div>
+        <nav className="footer__links">
+          <a
+            className="footer__link footer__link_practicum"
+            href="https://practicum.yandex.ru"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Яндекс.Практикум
+          </a>
+          <a
+            className="footer__link footer__link_github"
+            href="https://github.com/tatiana-dorokhova"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </nav>
+      </div>
+    );
+  }
+
+  return <></>;
 }
 
 export default Footer;
