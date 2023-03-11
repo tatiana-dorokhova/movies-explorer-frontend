4336138a897af71b45e684166337575f5b530d71
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-03-11 19:30:29 +0300

    links maked in portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,30 +7,32 @@ function Portfolio(props) {
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
         <li className="portfolio__item">
-          <div>Статичный сайт</div>
           <a
             className="portfolio__link"
             href="https://tatiana-dorokhova.github.io/how-to-learn/"
           >
-            ↗
+            <p className="portfolio__link-text">Статичный&nbsp;сайт</p>
+            <span>↗</span>
           </a>
         </li>
+
         <li className="portfolio__item">
-          <div>Адаптивный сайт</div>
           <a
             className="portfolio__link"
             href="https://tatiana-dorokhova.github.io/russian-travel/"
           >
-            ↗
+            <p className="portfolio__link-text">Адаптивный&nbsp;сайт</p>
+            <span>↗</span>
           </a>
         </li>
+
         <li className="portfolio__item">
-          <div>Одностраничное приложение</div>
           <a
             className="portfolio__link"
             href="https://mesto.dorokhova.nomoredomains.work"
           >
-            ↗
+            <p className="portfolio__link-text">Одностраничное приложение</p>
+            <span>↗</span>
           </a>
         </li>
       </ul>
