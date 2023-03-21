e3d3a2d4f2fbdd5d677d31a0976849e60c2d2089
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-03-21 22:57:50 +0300

    add search in savedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -4,12 +4,29 @@ import { initialSavedMovies } from '../../utils/initialMovies';
 
 import React, { useState, useEffect } from 'react';
 import { api } from '../../utils/MainApi';
+import MoviesSearchErrors from '../MoviesNotFound/MoviesSearchErrors';
 
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import { findMoviesBySearchQuery } from '../../utils/MoviesHandler';
 
 function SavedMovies(props) {
-  const [savedMovies, setSavedMovies] = useState([]);
+  const [savedMovies, setSavedMovies] = useState(initialSavedMovies);
+  const [lastSearchQuery, setLastSearchQuery] = useState('');
+  const [isShortFilmsOn, setIsShortFilmsOn] = useState(false);
+
+  useEffect(() => {
+    // при монтировании компонента достать данные из local storage
+    const initialSearchQuery = localStorage.getItem('searchQuery');
+    const initialShortFilms = localStorage.getItem('shortFilms');
+    // если данные есть, записать их в переменные состояния
+    if (initialSearchQuery) {
+      setLastSearchQuery(JSON.parse(initialSearchQuery));
+    }
+    if (initialShortFilms) {
+      setIsShortFilmsOn(JSON.parse(initialShortFilms));
+    }
+  }, []);
 
   useEffect(() => {
     api
@@ -26,10 +43,39 @@ function SavedMovies(props) {
     setSavedMovies(movies);
   }
 
+  const handleSearch = ({ searchQuery, shortFilms }) => {
+    // записать в localStorage фильтр и значение свитчера
+    localStorage.setItem('searchQuery', JSON.stringify(searchQuery));
+    localStorage.setItem('shortFilms', JSON.stringify(shortFilms));
+
+    // поменять переменные состояния
+    setLastSearchQuery(searchQuery);
+    setIsShortFilmsOn(shortFilms);
+  };
+
+  const moviesBySearchQuery = findMoviesBySearchQuery({
+    movies: savedMovies,
+    searchQuery: lastSearchQuery,
+    shortFilms: isShortFilmsOn,
+  });
+
   return (
     <>
-      <SearchForm />
-      <MoviesCardList movies={savedMovies} onChangeSavedMovies={handleChangeSavedMovies} />
+      <SearchForm
+        onSubmit={handleSearch}
+        lastSearchQuery={lastSearchQuery}
+        isShortFilmsOn={isShortFilmsOn}
+      />
+      {moviesBySearchQuery.length !== 0 && (
+        <MoviesCardList
+          movies={moviesBySearchQuery}
+          onChangeSavedMovies={handleChangeSavedMovies}
+        />
+      )}
+
+      {moviesBySearchQuery.length === 0 && lastSearchQuery && (
+        <MoviesSearchErrors errorText="Ничего не найдено" />
+      )}
     </>
   );
 }
